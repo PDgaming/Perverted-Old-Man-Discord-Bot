29d5ee97c2912761a842865dbcaeffa38b750e16
PD <89239979+PDgaming@users.noreply.github.com>
2025-03-25 09:26:36 +0530

    initial commit

---
+++ responses.py
@@ -0,0 +1,18 @@
+from random import choice, randint
+
+
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == "":
+        return "Bitch STFU."
+    elif "ban" in lowered:
+        return "Bitch STFU."
+    elif "mute" in lowered:
+        return "Bitch STFU."
+    elif "wtf" in lowered:
+        return "Bitch STFU."
+    elif "stop" in lowered:
+        return "Bitch STFU."
+    else:
+        return "Bitch STFU."
